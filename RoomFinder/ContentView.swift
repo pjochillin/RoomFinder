e4382b646fe4e3d695907c37a070eedcd85817e7
pjochillin <pjochillin@gmail.com>
2023-11-18 00:07:33 -0500

    Initial Commit

---
+++ RoomFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoomFinder
+//
+//  Created by Joshua Ochalek on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoomFinder/RoomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomFinderApp.swift
+//  RoomFinder
+//
+//  Created by Joshua Ochalek on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct RoomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
